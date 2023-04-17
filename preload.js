85144ccbed055ef19b61ee7cbe330c7aec0eec83
Zombi <zombeifedgaming@outlook.com>
2023-04-17 00:09:22 -0500

    Update

--- preload.js
+++ preload.js
@@ -4,5 +4,13 @@ window.path = loadLib('path')
 window.fs = loadLib('fs')
 
 window.addEventListener("DOMContentLoaded", ()=>{
+
+    for (let i = 0; i < 117; i++){
+        const programs = document.getElementById("programs")
+        const program = document.createElement("button")
+        program.setAttribute("id", "prgm"+(i+1))
+        program.setAttribute("class", "prgmBtn")
+        programs.append(program)
+    }
     loadFile("./plugins/index.js")
 })
