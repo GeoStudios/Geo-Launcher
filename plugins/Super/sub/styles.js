0ec6f1b39a4b5d21f553b362362957a41014d7ae
Zombi <zombeifedgaming@outlook.com>
2023-04-23 01:12:49 -0500

    movearoundconfig

--- plugins/Super/sub/styles.js
+++ plugins/Super/sub/styles.js
@@ -1,7 +1,7 @@
 function filepath(file2){
     return path.join(path.dirname(__filename), file2)
 }
-const config = loadFile(filepath("../config.json"))
+const config = loadFile(filepath("../../../config/Super/styles.json"))
 
 const style = document.getElementsByTagName("link")[0]
 style.setAttribute("href", filepath("../styles/opt"+config.style+".css"))
