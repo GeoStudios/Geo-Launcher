7e042adc0847211c4cb248e635ef7d0e6692a3bc
Zombi <zombeifedgaming@outlook.com>
2023-05-01 20:55:40 -0500

    Program Loader now loads into the Large Program Loader

--- plugins/programs/pack.js
+++ plugins/programs/pack.js
@@ -1,10 +1,26 @@
 const { RunExecutable } = LoadLib("Super/libs/cmd")
 
-function ClaimProgramSlot(programSlot, ProgramInfo, programCommend){
+function ClaimProgramSlot(programSlot, ProgramInfo, ProgramCommand){
 
     const prg=document.getElementById(`prgm${programSlot}`)
     prg.getElementsByTagName("img")[0].src = `../${ProgramInfo.ContentLogo.small_188}`
-    prg.addEventListener("click", programCommend)
+    const Program = () => {
+        ProgramCommand(ProgramInfo)
+    }
+    prg.addEventListener("click", Program)
+
+}
+
+function LoadProgramIntoLarge(ProgramInfo){
+
+    const g = document.getElementById("game")
+    g.src = `../${ProgramInfo.ContentLogo.large_200}`
+    const p = document.getElementById("playbtn")
+    p.innerText = `Play ${ProgramInfo.Name}`
+    p.addEventListener("click", ()=>{
+        RunExecutable(ProgramInfo.startCommand)
+    })
+    document.getElementById("GameTitle").innerText = ProgramInfo.Name
 
 }
 
@@ -12,9 +28,10 @@ ClaimProgramSlot(1,
     {
         ContentLogo:{
             small_188:"programs/program1/program.png",
-            large_200:""
+            large_200:"programs/program1/program.png"
         },
+        Name:"MiniCal",
         DownloadUrl:"",
-        startCommand:""
+        startCommand:"java -jar programs/program1/SuperCal.jar"
     }
-    , RunExecutable("java -jar programs/program1/SuperCal.jar"))
+    , LoadProgramIntoLarge)
